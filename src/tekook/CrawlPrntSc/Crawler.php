40ae66d014419619ff6db95c7880f8cfce56c2b7
Julian Tekook <julian@tekook.de>
2020-03-03 22:04:26 +0100

    "pretty" it up.

--- src/tekook/CrawlPrntSc/Crawler.php
+++ src/tekook/CrawlPrntSc/Crawler.php
@@ -7,13 +7,36 @@ use Illuminate\Support\Str;
 
 class Crawler
 {
-    const BASE    = 'https://prnt.sc';
+    /**
+     * Base Url of Prnt.Sc
+     */
+    const BASE = 'https://prnt.sc';
+    /**
+     * Url's to ignore since they are 404
+     */
     const IGNORES = [
         "//st.prntscr.com/2020/02/10/0334/img/0_173a7b_211be8ff.png",
     ];
-    const REG     = '/\<meta property=\"og\:image\" content="([^"]+)\"\/\>/mi';
+    /**
+     * RegEx to grab image url
+     */
+    const REG = '/\<meta property=\"og\:image\" content="([^"]+)\"\/\>/mi';
+    /**
+     *  UserAgent to present to WebPage.
+     *
+     * @var string
+     */
     public string $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36 Edg/80.0.361.62';
+    /**
+     * @var string Directory to output to
+     */
+    public string $output = "/app/output";
 
+    /**
+     * Execute the crawler.
+     *
+     * @param int $max Maximum images to crawl. (404s will not be counted!)
+     */
     public function execute(int $max = 10): void
     {
         $client = new Client([
@@ -23,9 +46,8 @@ class Crawler
             ],
         ]);
         for ($i = 0; $i < $max; $i++) {
-            $rnd = Str::lower(Str::random('6'));
-            //$rnd = "ab5540";
-            $response = $client->get($rnd);
+            $name = Str::lower(Str::random('6'));
+            $response = $client->get($name);
             $body = (string)$response->getBody();
             if (preg_match(self::REG, $body)) {
                 preg_match_all(self::REG, $body, $matches, PREG_SET_ORDER, 0);
@@ -34,12 +56,35 @@ class Crawler
                     $i--;
                     continue;
                 }
-                $path = '/app/output/' . time() . '_' . $rnd . '.png';
-                $r = $client->get($url);
-                file_put_contents($path, $r->getBody());
-                echo('downloaded ' . $rnd . '\r\n');
+                $response = $client->get($url);
+                file_put_contents($this->getOutputPath($name), $response->getBody());
+                $this->log('downloaded ' . $name);
                 usleep(500);
+            } else {
+                $i--;
             }
         }
+    }
+
+    /**
+     * Get output path for an image name.
+     *
+     * @param $name
+     *
+     * @return string
+     */
+    protected function getOutputPath($name)
+    {
+        return $this->output . DIRECTORY_SEPARATOR . time() . '_' . $name . '.png';
+    }
+
+    /**
+     * log to STDOUT
+     *
+     * @param $text
+     */
+    protected function log($text)
+    {
+        echo $text . '\r\n';
     }
 }
