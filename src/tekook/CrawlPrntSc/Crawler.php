84ce3538474ceeb396ca032de6a91146fc41148f
Julian Tekook <julian@tekook.de>
2020-03-03 21:55:49 +0100

    require php and extract more

--- src/tekook/CrawlPrntSc/Crawler.php
+++ src/tekook/CrawlPrntSc/Crawler.php
@@ -11,23 +11,24 @@ class Crawler
     const IGNORES = [
         "//st.prntscr.com/2020/02/10/0334/img/0_173a7b_211be8ff.png",
     ];
+    const REG     = '/\<meta property=\"og\:image\" content="([^"]+)\"\/\>/mi';
+    public string $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36 Edg/80.0.361.62';
 
-    public function execute(int $max = 10) : void
+    public function execute(int $max = 10): void
     {
         $client = new Client([
             'base_uri' => self::BASE,
             'headers'  => [
-                'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36 Edg/80.0.361.62',
+                'User-Agent' => $this->userAgent,
             ],
         ]);
-        $reg = '/\<meta property=\"og\:image\" content="([^"]+)\"\/\>/mi';
         for ($i = 0; $i < $max; $i++) {
             $rnd = Str::lower(Str::random('6'));
             //$rnd = "ab5540";
             $response = $client->get($rnd);
             $body = (string)$response->getBody();
-            if (preg_match($reg, $body)) {
-                preg_match_all($reg, $body, $matches, PREG_SET_ORDER, 0);
+            if (preg_match(self::REG, $body)) {
+                preg_match_all(self::REG, $body, $matches, PREG_SET_ORDER, 0);
                 $url = $matches[0][1];
                 if (in_array($url, self::IGNORES)) {
                     $i--;
